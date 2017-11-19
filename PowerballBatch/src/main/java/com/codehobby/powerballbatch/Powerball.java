190f82524f06d8d8a8bfeaf6f7f97c955ee12c7e
Jeff <github@314ghz.com>
2017-11-18 20:42:58 -0600

    Added a toString method to the Powerball class.

--- PowerballBatch/src/main/java/com/codehobby/powerballbatch/Powerball.java
+++ PowerballBatch/src/main/java/com/codehobby/powerballbatch/Powerball.java
@@ -1,5 +1,6 @@
 package com.codehobby.powerballbatch;
 
+import java.text.SimpleDateFormat;
 import java.util.Calendar;
 
 public class Powerball
@@ -48,5 +49,45 @@ public class Powerball
 	public Calendar getDate()
 	{
 		return date;
+	}
+
+	/**
+	 * Returns a string in the format "number type MM-dd-yyyy".
+	 * 
+	 * @return A string in the format "number type MM-dd-yyyy"
+	 */
+	public String toString()
+	{
+		StringBuilder toStringSB = new StringBuilder();
+
+		//The number
+		toStringSB.append( number );
+		toStringSB.append( " " );
+
+		//The type
+		switch( type )
+		{
+			case WHITE_BALL:
+				toStringSB.append( "White Ball" );
+				break;
+
+			case POWERBALL:
+				toStringSB.append( "Powerball" );
+				break;
+
+			case POWER_PLAY:
+				toStringSB.append( "Power Play" );
+				break;
+
+			default:
+				toStringSB.append( "Unkown Type" );
+		}
+		toStringSB.append( " " );
+
+		//The date
+		SimpleDateFormat dateFormat = new SimpleDateFormat("MM-dd-yyyy");
+		toStringSB.append( dateFormat.format(date.getTime()) );
+
+		return toStringSB.toString();
 	}
 }
