6d64023cac3cf68bfab23214d6bf8285669e0a7c
Jeff <github@314ghz.com>
2017-11-18 19:45:38 -0600

    Initial setup of the batch job using Spring Boot.

---
+++ PowerballBatch/src/main/java/com/codehobby/powerballbatch/PowerballBatchApplication.java
@@ -0,0 +1,12 @@
+package com.codehobby.powerballbatch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PowerballBatchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PowerballBatchApplication.class, args);
+	}
+}
